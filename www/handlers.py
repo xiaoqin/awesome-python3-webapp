34f189056606b36e2bfebecbddc0c1290cd3c42b
xiaoqin <843006707@qq.com>
2017-12-05 01:01:49 +0800

    day 09

--- www/handlers.py
+++ www/handlers.py
@@ -11,9 +11,10 @@ from coroweb import get, post
 
 from models import User, Comment, Blog, next_id
 
+
 @get('/')
 async def index(request):
-    summary = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+    summary = 'test summary'
     blogs = [
         Blog(id='1', name='Test Blog', summary=summary, created_at=time.time()-120),
         Blog(id='2', name='Something New', summary=summary, created_at=time.time()-3600),
@@ -23,3 +24,10 @@ async def index(request):
         '__template__': 'blogs.html',
         'blogs': blogs
     }
+
+@get('/api/users')
+async def api_get_users():
+    users = await User.findAll(orderBy='created_at desc')
+    for u in users:
+        u.passwd = '******'
+    return dict(user=users)
